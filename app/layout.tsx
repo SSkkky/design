63775287c307f00f5aeb0a869663973e62997d2f
SSkkky <worte5633@gmail.com>
2025-07-01 03:38:40 +0900

    [update] 전역 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="kr">
-      <body className={`${pretendard.variable} font-sans tracking-tight`}>
+      <body className={`${pretendard.className} font-sans tracking-tight`}>
         <div className="min-h-screen bg-white text-black">
           {children}
         </div>
