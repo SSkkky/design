cb20457cc404fee79ff837d362be0dbe6858c7f8
SSkkky <worte5633@gmail.com>
2025-07-08 04:54:15 +0900

    [add] footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,6 +5,8 @@ import gsap from 'gsap';
 
 export default function Footer() { 
   return (
-    <></>
+    <footer className='bg-gray-200 p-2 text-center' role="contentinfo">
+      <p className='text-sm'>Copyright 2025. Son Haneul All rights reserved.</p>
+    </footer>
   );
 }
